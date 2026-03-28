f3a806bd5e5b06dd22e44d22e2f16972e4000bd2
Xinyan He <3104719H@student.gla.ac.uk>
2026-03-28 23:14:00 +0000

    Fix manage.py module name and build_files.sh python3 command

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+#!/bin/bash
+set -e
+export DJANGO_SETTINGS_MODULE=Project.settings
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
